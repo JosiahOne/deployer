0366116f7b50f6483a463b718a65967d91b8331c
Josiah Bruner <jsbruner@umich.edu>
2020-07-17 00:00:16 -0400

    Add HSTS test

--- deploymentTests/3-hsts.sh
+++ deploymentTests/3-hsts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 EXPECTEDHSTS="Strict-Transport-Security: max-age=31536000; includeSubDomains; preload"
-SITEHSTS="$(curl -si https://invoicer.securing-devops.com/ |grep Strict-Transport-Security | tr -d '\r\n' )"
+SITEHSTS="$(curl -si https://invoicer.riskytrees.com/ |grep Strict-Transport-Security | tr -d '\r\n' )"
 if [ "${SITEHSTS}" == "${EXPECTEDHSTS}" ]; then
     echo "HSTS header matches expectation"
     exit 0
