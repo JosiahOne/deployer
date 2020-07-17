bf01f3444bcb5e0057637886a2e5b271c63d8efa
Josiah Bruner <jsbruner@umich.edu>
2020-07-17 00:03:12 -0400

    Add key pinning test

--- deploymentTests/4-hpkp.sh
+++ deploymentTests/4-hpkp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 EXPECTEDHPKP='Public-Key-Pins: max-age=1296000; includeSubDomains; pin-sha256="YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg="; pin-sha256="++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI="'
-SITEHPKP="$(curl -si https://invoicer.securing-devops.com/ |grep Public-Key-Pins | tr -d '\r\n' )"
+SITEHPKP="$(curl -si https://invoicer.riskytrees.com/ |grep Public-Key-Pins | tr -d '\r\n' )"
 if [ "${SITEHPKP}" == "${EXPECTEDHPKP}" ]; then
     echo "HSTS header matches expectation"
     exit 0
